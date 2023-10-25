3863af694e3679b115b844b87d493b3a8e64ea5d
onowdev <onowdev@gmail.com>
2023-10-25 13:48:12 +0700

    golang boolean

--- basic/boolean.go
+++ basic/boolean.go
@@ -6,4 +6,18 @@ func main() {
 
 	var exsist bool = true
 	fmt.Println("Exsist? %t \n", exsist)
+
+	printMessage := true
+
+	if printMessage {
+		fmt.Println("Pesan")
+		fmt.Println("Ayo Belajar")
+	}
+
+	accountBallance := 800
+	accountCredit := 9000
+
+	if accountBallance+accountCredit > 2000 {
+		fmt.Println("Anda Punya uang untuk di belanja kan")
+	}
 }
