9dc13bad2f7640df5558b51762834f2b1c4291e4
onowdev <onowdev@gmail.com>
2023-11-15 12:36:01 +0700

    add import statement

---
+++ basic/imports/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Hasil dari akar %g 9 adalah.\n", math.Sqrt(9))
+}
+
+/*
+This code groups the imports into a parenthesized, "factored" import statement.
+
+You can also write multiple import statements, like:
+
+import "fmt"
+import "math"
+But it is good style to use the factored import statement.
+*/
